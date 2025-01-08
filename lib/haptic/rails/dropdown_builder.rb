78be8f16f87f872015771279fcbe348743a06bb4
dmgoeller <denis@dmgoeller.de>
2025-01-08 07:33:05 +0100

    Work in progress

--- lib/haptic/rails/dropdown_builder.rb
+++ lib/haptic/rails/dropdown_builder.rb
@@ -8,14 +8,18 @@ module Haptic
       end
 
       def toggle(content = nil, options = {}, &block)
+        content, options = nil, content if content.is_a?(Hash)
+
         options = options.merge(
+          is: nil,
           type: 'button',
           class: [options[:class], 'toggle', 'haptic-field']
         )
-        @builder.content_tag('button', content, options, &block)
+        @builder.button_tag(content, options, &block)
       end
 
       def popover(content = nil, options = {}, &block)
+        content, options = nil, content if content.is_a?(Hash)
         options = options.merge(class: [options[:class], 'popover'])
 
         @builder.content_tag('div', content, options, &block)
