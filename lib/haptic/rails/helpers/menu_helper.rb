9b1a1d7b92aedbe4289b0395ce75db0b2c5569a4
dmgoeller <denis@dmgoeller.de>
2026-03-11 20:11:07 +0100

    Allow menus to be opened to top

--- lib/haptic/rails/helpers/menu_helper.rb
+++ lib/haptic/rails/helpers/menu_helper.rb
@@ -4,7 +4,25 @@ module Haptic
   module Rails
     module Helpers
       module MenuHelper
+        # Creates a <code><haptic-dropdown-menu></code> tag.
+        #
+        # # ==== Options
+        #
+        # - <code>:open_to_top</code> - If is <code>true</code>, the menu
+        #   pops up to top instead of to bottom.
+        #
+        # # ==== Example
+        #
+        #   haptic_dropdown_menu do |dropdown|
+        #     dropdown.menu do |menu|
+        #       menu.item 'Duplicate', href: '/duplicate'
+        #       menu.item 'Delete', href: '/delete'
+        #     end
+        #   end
         def haptic_dropdown_menu(options = {})
+          options = options.stringify_keys
+          options['open-to-top'] = '' if options.delete('open_to_top')
+
           haptic_dropdown_menu_tag(options) do
             yield DropdownMenuBuilder.new(self) if block_given?
           end
--- test/haptic/rails/helpers/menu_helper_test.rb
+++ test/haptic/rails/helpers/menu_helper_test.rb
@@ -9,25 +9,7 @@ module Haptic
         include MenuHelper
         include TagHelper
 
-        def test_haptic_dropdown_menu_with_block
-          assert_dom_equal(
-            <<~HTML,
-              <haptic-dropdown-menu>
-                <haptic-menu class="popover">
-                  <a is="haptic-menu-item" href="/">Home</a>
-                </haptic-menu>
-                <div class="backdrop"></div>
-              </haptic-dropdown-menu>
-            HTML
-            haptic_dropdown_menu do |dropdown|
-              dropdown.menu do |menu|
-                menu.item 'Home', href: '/'
-              end
-            end
-          )
-        end
-
-        def test_haptic_dropdown_menu_without_block
+        def test_haptic_dropdown_menu
           assert_dom_equal(
             <<~HTML,
               <haptic-dropdown-menu>
@@ -35,6 +17,35 @@ module Haptic
               </haptic-dropdown-menu>
             HTML
             haptic_dropdown_menu
+          )
+        end
+
+        def test_haptic_dropdown_menu_with_options
+          assert_dom_equal(
+            <<~HTML,
+              <haptic-dropdown-menu open-to-top>
+                <div class="backdrop"></div>
+              </haptic-dropdown-menu>
+            HTML
+            haptic_dropdown_menu(open_to_top: true)
+          )
+        end
+
+        def test_haptic_dropdown_menu_with_block
+          assert_dom_equal(
+            <<~HTML,
+              <haptic-dropdown-menu>
+                <haptic-menu class="popover">
+                  <a is="haptic-menu-item" href="/duplicate">Duplicate</a>
+                </haptic-menu>
+                <div class="backdrop"></div>
+              </haptic-dropdown-menu>
+            HTML
+            haptic_dropdown_menu do |dropdown|
+              dropdown.menu do |menu|
+                menu.item 'Duplicate', href: '/duplicate'
+              end
+            end
           )
         end
       end
